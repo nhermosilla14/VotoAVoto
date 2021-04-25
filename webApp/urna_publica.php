ab5904c8ff7ef2817570d5b648cd7bc3df5cdf23
Nicolás Hermosilla P <nhermosilla14@gmail.com>
2021-04-25 17:27:02 -0400

    Added public repo in its basic form

---
+++ webApp/urna_publica.php
@@ -0,0 +1,9 @@
+<?php
+
+$fileList = glob('*');
+foreach($fileList as $filename){
+    //Use the is_file function to make sure that it is not a directory.
+    if(is_file($filename)){
+        echo $filename, '<br>'; 
+    }   
+}
