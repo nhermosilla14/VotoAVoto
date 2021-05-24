e9a28818983eca6785ca31f6bbc8cd65f0e84bf9
litneet64 <litneet64@gmail.com>
2021-05-23 23:31:46 -0400

    chore: apply new branding

--- webApp/urna_publica.php
+++ webApp/urna_publica.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>CEE-ELO UTFSM</title>
+        <title>TRICEL UTFSM</title>
         <meta name="viewport" content="width=device-width,initial-scale=1"/>
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link rel="stylesheet" href="./css/style.css">
@@ -16,12 +16,12 @@
             <div class="w3-col l3 m1 s1 w3-container">
             </div>
             <div class="form w3-col l6 m10 s10">
-                    <h1>Elecciones CEE-ELO 2021</h1>
+                    <h1>Elecciones TRICEL 2021</h1>
                     <h2>Urna Virtual Pública</h2>
                     <div class="alarm">
                         <i class="fas fa-info-circle">
                             <h3>Acá puedes consultar todos los votos que han sido emitidos, con un delay de 15 minutos (por anonimidad).</h3>
-                            <h4>Los votos son renombrados a su suma de chequeo SHA256, y recomendamos usarla si quieres verificar que tu voto está subido. Más información sobre esto y sobre cómo garantizamos tu anonimidad en cee-elo.cl/faq.html.</h4>
+                            <h4>Los votos son renombrados a su suma de chequeo SHA256, y recomendamos usarla si quieres verificar que tu voto está subido. Más información sobre esto y sobre cómo garantizamos tu anonimidad en faq.html.</h4>
                         </i>
                     </div>
                     <div id="wrapper">
@@ -32,8 +32,8 @@
                         foreach($fileList as $filename){
                             //Use the is_file function to make sure that it is not a directory.
                             if(is_file($filename)){
-                                echo "<p><a href=$protocol$domain$filename>", basename($filename), "</a> </p>"; 
-                            }   
+                                echo "<p><a href=$protocol$domain$filename>", basename($filename), "</a> </p>";
+                            }
                         }
                     ?>
                     </div>
@@ -51,7 +51,7 @@
                             <div class="alarm">
                                 <i class="fas fa-rocket">
                                     <h3>¿Cómo bajo todos los archivos?</h3>
-                                    <h4>Con cualquier gestor de descargas: JDownloader, Download Master, o nuestro favorito: wget. Para este último, ejecuta: wget -np -nH -r https://cee-elo.cl/votaciones/urna_publica/ </h4>
+                                    <h4>Con cualquier gestor de descargas: JDownloader, Download Master, o nuestro favorito: wget. Para este último, ejecuta: wget -np -nH -r <?php echo $_SERVER["SERVER_NAME"] . "votaciones/urna_publica/"; ?> </h4>
                                 </i>
                             </div>
                         </div>
@@ -68,16 +68,16 @@
                             <button class="btn"><a href="./faq.html">Preguntas frecuentes</a></button>
                         </div>
                     </div>
-                    <div class="w3-row"> 
-                        
+                    <div class="w3-row">
+
                     </div>
                     <br>
-                    <image src= "./img/cee_elo_logo.png"> 
+                    <image src= "./img/cee_elo_logo.png">
             </div>
             <div class="w3-col l3 m1 s1 w3-container">
             </div>
         </div>
-        
+
 
 
     </body>
